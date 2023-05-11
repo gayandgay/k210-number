95b05e3aed3c4eec7aca1010a4baeeed5fc169f5
puzz <z15378560090@gmail.com>
2023-05-11 22:20:32 +0800

    On branch master Your branch is up to date with 'k210/master'.
    Changes to be committed:
    	new file:   BOOTEX.LOG
    	modified:   README.md
    	modified:   System Volume Information/IndexerVolumeGuid
    	modified:   System Volume Information/WPSettings.dat
    	deleted:    Uart.py
    	modified:   boot.py
    	deleted:    model-40708.kmodel
    	new file:   models/main.kmodel

--- boot.py
+++ boot.py
@@ -4,10 +4,24 @@
 import sensor, image, lcd, time
 import KPU as kpu
 import gc, sys
-# from machine import UART
-# from fpioa_manager import fm
-import Uart
+from machine import UART
+from fpioa_manager import fm
+# import Uart
 import time
+#from collections import Counter
+#from machine import SDCard
+
+#SDCard.remount()
+
+
+def UartInit():
+    fm.register(25, fm.fpioa.UART1_TX, force=True)
+    fm.register(24, fm.fpioa.UART1_RX, force=True)
+
+def UartSend(label, position):
+    uart_A = UART(UART.UART1, 115200, 8, 0, 1, timeout=1000, read_buf_len=4096)
+    # uart_A.write(label)
+    uart_A.write("{}:{}".format(position, label).encode('utf-8'))
 
 def lcd_show_except(e):
     import uio
@@ -15,6 +29,7 @@ def lcd_show_except(e):
     sys.print_exception(e, err_str)
     err_str = err_str.getvalue()
     img = image.Image(size=(224,224))
+
     img.draw_string(0, 10, err_str, scale=1, color=(0xff,0x00,0x00))
     lcd.display(img)
 
@@ -30,8 +45,10 @@ def main(anchors, labels = None,  sensor_window=(224, 224), model_addr="/sd/mode
     lcd.init(type=1)
     lcd.rotation(lcd_rotation)
     lcd.clear(lcd.WHITE)
+    labelDict = {}
 
-    Uart.UartInit()
+
+    UartInit()
     # fm.register(24, fm.fpioa.UART1_TX, force=True)
     # fm.register(25, fm.fpioa.UART1_RX, force=True)
     # fm.register(22, fm.fpioa.UART2_TX, force=True)
@@ -68,14 +85,25 @@ def main(anchors, labels = None,  sensor_window=(224, 224), model_addr="/sd/mode
             if objects:
                 for obj in objects:
                     pos = obj.rect()
+                    labelDict[obj.classid()] = pos
                     img.draw_rectangle(pos)
                     img.draw_string(pos[0], pos[1], "%s : %.2f" %(labels[obj.classid()], obj.value()), scale=2, color=(255, 0, 0))
-                    if labels[obj.classid()] != "netLine" :
-                        Uart.UartSend(labels[obj.classid()],pos[0])
-                    else :
-                        Uart.UartSend(labels[obj.classid()],pos[1])
-                    # Uart.UartSend(labels[obj.classid()])
-                    #time.sleep(0.001)
+
+                    #if len(labelDict) == len(labels):
+                print(len(labelDict),len(objects))
+                sorted_Label = sorted(labelDict.items(), key=lambda x: x[1])
+                sorted_list = list(sorted_Label)
+                if len(labelDict) >= 2:
+                    for i,target in enumerate(sorted_list):
+                        UartSend(target[0]+1, i)
+                        time.sleep(0.001)
+                        #UartSend(sorted_list[1][0], "right")
+                elif len(labelDict) == 2:
+                    i += 1
+                    if i >= 3:
+                        i = 0;
+                        UartSend(target[0]+1, i)
+            labelDict.clear()
             img.draw_string(0, 200, "t:%dms" %(t), scale=2, color=(255, 0, 0))
             lcd.display(img)
     except Exception as e:
@@ -89,9 +117,9 @@ if __name__ == "__main__":
         #labels = ['1', '2', '3', '4', '5', '6', '7', '8']
         #anchors = [1.40625, 1.8125000000000002, 5.09375, 5.28125, 3.46875, 3.8124999999999996, 2.0, 2.3125, 2.71875, 2.90625]
         #main(anchors = anchors, labels=labels, model_addr=0x300000, lcd_rotation=2, sensor_window=(224, 224))
-        labels = ['1', '2', '3', '4', '5', '6', '7', '8']
-        anchors = [2.16, 2.84, 0.55, 0.87, 3.05, 3.16, 1.13, 1.53, 1.91, 1.97]
-        main(anchors = anchors, labels=labels, model_addr="/sd/models/m2.kmodel", lcd_rotation=2, sensor_window=(224, 224), sensor_hmirror=True)
+        labels = ['3', 'netline', '8', '1', '4', '6', '5', '7', '2']
+        anchors = [1.34, 1.59, 1.84, 2.22, 2.56, 4.28, 1.09, 1.2, 6.83, 2.42]
+        main(anchors = anchors, labels=labels, model_addr="/sd/models/main.kmodel", lcd_rotation=2, sensor_window=(224, 224), sensor_hmirror=True)
     except Exception as e:
         sys.print_exception(e)
         lcd_show_except(e)
